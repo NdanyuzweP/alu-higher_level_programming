887419b81de603afcc2628f16c625825511c3437
Prince Ndanyuzwe <122270300+NdanyuzweP@users.noreply.github.com>
2023-02-09 20:59:56 +0200

    Update 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,1 +1,10 @@
-ad
+#!/usr/bin/python3
+def uppercase(str):
+    new = ""
+    for char in str:
+        if ord('a') <= ord(char) <= ord('z'):
+            char = ord(char) - 32
+            new += chr(char)
+        else:
+            new += char
+    print("{}".format(new))
