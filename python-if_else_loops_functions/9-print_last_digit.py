d81469a3037bd951410f896926f67cb4ef9eae8a
Prince Ndanyuzwe <122270300+NdanyuzweP@users.noreply.github.com>
2023-02-09 21:00:26 +0200

    Update 9-print_last_digit.py

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,1 +1,8 @@
-ede
+#!/usr/bin/python3
+def print_last_digit(number):
+    if number > 0:
+        last = number % 10
+    else:
+        last = (number * -1) % 10
+    print("{:d}".format(last), end="")
+    return(last)
