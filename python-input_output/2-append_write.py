289c3e01eaf534225dd150f9153a29607fde4635
NdanyuzweP <p.ndanyuzwe@alustudent.com>
2023-03-23 17:49:29 +0200

    df

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" a func to append text to filename provided"""
+"""Defines a file-appending function."""
 
 
 def append_write(filename="", text=""):
-    """a new definition to append text to filename
-    and return num of text
+    """Appends a string to the end of a UTF8 text file.
+    Args:
+        filename (str): The name of the file to append to.
+        text (str): The string to append to the file.
+    Returns:
+        The number of characters appended.
     """
-
-    with open(filename, 'a') as f:
-        f.write(text)
-
-    return len(text)
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
