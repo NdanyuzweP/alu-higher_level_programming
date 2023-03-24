dffe866d913a8a16ef174c91c468418217a9f6e8
NdanyuzweP <p.ndanyuzwe@alustudent.com>
2023-03-24 22:43:08 +0200

    xcx

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""Defines a file-writing function."""
+""" mmodule to open file and append to it"""
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file.
-    Args:
-        filename (str): The name of the file to write.
-        text (str): The text to write to the file.
-    Returns:
-        The number of characters written.
+    """function to define a counter
+    and it will write text to file
     """
-    with open(filename, "w", encoding="utf-8") as f:
-        return f.write(text)
+
+    with open(filename, 'w+') as f:
+        f.write(text)
+
+    return len(text)
--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""Defines a string-to-JSON function."""
+"""Json to string."""
+
+
 import json
 
 
 def to_json_string(my_obj):
-    """Return the JSON representation of a string object."""
+    """json to a string."""
     return json.dumps(my_obj)
