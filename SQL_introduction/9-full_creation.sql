cdf9cf37d4477d1e02669a792685ffcdbe9f4c01
NdanyuzweP <p.ndanyuzwe@alustudent.com>
2023-03-29 16:08:00 +0200

    ds

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,11 @@
--- creating second table in database
-CREATE TABLE second_table(
+-- creating table and inserting some ata the same time
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT);
 
 INSERT INTO second_table (id, name, score)
-VALUES ((1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8));
+VALUES (1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George',8);
