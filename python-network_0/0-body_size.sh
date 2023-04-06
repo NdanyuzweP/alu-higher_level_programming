84ae9eca009b8f31e0fce72dc73520f53caf9402
NdanyuzweP <p.ndanyuzwe@alustudent.com>
2023-04-07 01:30:52 +0200

    df

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# ends a request to that URL displays the size of the response body
+#sends a request to that URL displays the size of the response body
 curl -sI "$1" | awk '/Content-length/{print $2}'
